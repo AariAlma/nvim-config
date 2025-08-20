f436b21d125e55ca699e7b7c4ecc63c2c2d5d4ec
AariAlma <aariyan@gmail.com>
2025-08-20 02:25:17 -0400

    Added mason-lsp config
    automates lsp configuration, but this is only avalable to nvim version > 0.11.0

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,49 +1,18 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		require("mason").setup({
-			PATH = "prepend",
-		})
-		-- Clangd
-		lspconfig.clangd.setup({
-			cmd = {
-				"clangd",
-				"--background-index",
-				"--pch-storage=memory",
-				"--all-scopes-completion",
-				"--pretty",
-				"--header-insertion=never",
-				"-j=4",
-				"--header-insertion-decorators",
-				"--function-arg-placeholders",
-				"--completion-style=detailed",
-			},
-			filetypes = { "c", "cpp", "objc", "objcpp" },
-			root_dir = require("lspconfig").util.root_pattern("src"),
-			init_option = { fallbackFlags = { "-std=c++2a" } },
-			capabilities = capabilities,
-			single_file_support = true,
-		})
-		-- Lua
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" }, -- Recognize 'vim' as a global variable
-					},
-					workspace = {
-						library = {
-							vim.api.nvim_get_runtime_file("", true),
-							"${3rd}/love2d/library",
-						}, -- Include Neovim runtime files
-					},
-					telemetry = {
-						enable = false,
-					},
-				},
-			},
-		})
-	end,
+	{
+		"neovim/nvim-lspconfig",
+	},
+	{
+		"mason-org/mason.nvim",
+		lazy = false,
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
 }
